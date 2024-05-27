e0c006cda0254da87bf9aafefbaca5d6ac7e9a88
BenMonroy <135274492+BenMonroy@users.noreply.github.com>
2024-05-27 19:28:20 -0400

    Add files via upload

---
+++ LAMP Stack/js/signin.js
@@ -0,0 +1,6 @@
+const wrapper = document.querySelector('.wrapper');
+const login = document.querySelector('.login-link');
+const register = document.querySelector('.signup-link');
+
+register.addEventListener('click', () => { wrapper.classList.add('active'); });
+login.addEventListener('click', () => { wrapper.classList.remove('active'); });
